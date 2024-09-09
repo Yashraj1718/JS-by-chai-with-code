b6f91c71ef1b65670c6b3cad214efd75b14f3f7c
Yashraj1718 <148371482+Yashraj1718@users.noreply.github.com>
2024-09-09 16:51:31 +0000

    let,const,var all the things

---
+++ test.js
@@ -0,0 +1,21 @@
+console.log("Yashraj");
+
+
+const accountId= 12345;
+let accountEmail = "yashraj@gmail.com";
+var accountPassword = "1718";
+accountCity = "Solapur";
+let accountState;
+
+
+accountEmail="yash1718@gmail.com";
+accountPassword="1817";
+accountCity="Pune";
+
+/*
+Note:- Prefer not to use var
+bcz of issue in block scope({}) and functional scope..
+block scope is curly brackets...
+*/
+
+console.table([accountEmail,accountPassword,accountId,accountState]); // this created the data in tabular format...
