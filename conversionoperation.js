68751d82e7353685bb777da37646362f96ea89e2
Yashraj1718 <148371482+Yashraj1718@users.noreply.github.com>
2024-09-09 17:32:13 +0000

    conversion operations

---
+++ conversionoperation.js
@@ -0,0 +1,104 @@
+// let score = "33abc"
+// console.log(typeof score); // output is string
+// console.log(typeof(score));// output is string
+
+// let valueNumber = Number(score)
+// console.log(typeof valueNumber);// output is number
+// console.log(valueNumber);// output is Nan(not a number)...
+
+
+
+
+
+// let score = null 
+// console.log(typeof score); // output is object
+// console.log(typeof(score));// output is object
+
+// let valueNumber = Number(score)
+// console.log(typeof valueNumber);// output is number
+// console.log(valueNumber);// output is 0
+
+
+
+
+// let score = undefined
+// console.log(typeof score); // output is undefined
+// console.log(typeof(score));// output is undefined
+
+// let valueNumber = Number(score)
+// console.log(typeof valueNumber);// output is number
+// console.log(valueNumber);// output is Nan(not a number)...
+
+
+
+
+
+
+// let score=true
+// console.log(typeof score); // output is boolean
+// console.log(typeof(score));// output is boolean
+
+// let valueNumber = Number(score)
+// console.log(typeof valueNumber);// output is number
+// console.log(valueNumber);// output is 1
+
+
+
+
+
+// let score = "Yashraj"
+// console.log(typeof score); // output is string
+// console.log(typeof(score));// output is string
+
+// let valueNumber = Number(score)
+// console.log(typeof valueNumber);// output is number
+// console.log(valueNumber);// output is Nan(not a number)...
+
+
+
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+
+
+
+
+
+// let isLoggedIn = 1
+// let booleanIsLoogedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoogedIn); // output is true bcz value of isLoggedIn is 1..
+
+
+
+
+
+// let isLoggedIn = ""
+// let booleanIsLoogedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoogedIn); // output is false bcz value of isLoggedIn is null which means by default it is 0..
+
+
+
+
+// let isLoggedIn = "Yashraj"
+// let booleanIsLoogedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoogedIn); // output is true bcz value of isLoggedIn is not 0 or null..
+
+
+
+// 1 => true; 0=> false
+// "" => false
+//"Yashraj" => true
+
+
+
+
+// let someNumber = 17
+// let stringNumber = String(someNumber)
+// console.log(stringNumber); // output is 17
+// console.log(typeof stringNumber); // output is string
+
+
